4a8380e3e2f0206753ecfe7176a8a8c290f35cb0
yo1995 <cht@bupt.edu.cn>
2018-07-27 20:05:08 +0800

    new busuanzi pv counter

--- ThreeBody/React-source/src/components/Loader.js
+++ ThreeBody/React-source/src/components/Loader.js
@@ -10,7 +10,12 @@ class Loader extends Component {
 			<div id="loader">✡</div>
 			<div className="loader-section section-left"></div>
 			<div className="loader-section section-right"></div>
-			<div className="load_title"><a href="https://github.com/yo1995/Daily_Web_Tasks/ThreeBody">消灭人类暴政<br/>世界属于三体</a><br/><small>*请驾驶高速飞船以逃离二向箔</small></div>
+			<div className="load_title">
+				<a href="https://github.com/yo1995/Daily_Web_Tasks/ThreeBody">消灭人类暴政<br/>世界属于三体</a><br/>
+				<small>*请驾驶高速飞船以逃离二向箔</small><br/>
+				<span id="busuanzi_container_site_pv">PV: <span id="busuanzi_value_site_pv"></span></span>
+				<span id="busuanzi_container_site_uv">UV: <span id="busuanzi_value_site_uv"></span></span>
+			</div>
 		</div>
     )
   }
